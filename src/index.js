2a81e017249ff1e8cf3f1c600e5c51e75c8c8152
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2023-04-02 10:49:02 +0700

    feat: config redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import store from './store'
+
+window.store = store
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
